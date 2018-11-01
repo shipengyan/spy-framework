84b7ad33046ec34d2ab06d26ffa48f7deb60c48e
shi.pengyan <spy19881221@163.com>
2018-11-01 17:09:28 +0800

    cors filter

---
+++ src/main/java/com/github/shipengyan/framework/web/filter/CorsFilter.java
@@ -0,0 +1,43 @@
+package com.github.shipengyan.framework.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 模块名
+ *
+ * @author spy
+ * @version 1.0 2018/11/1
+ * @since 1.0
+ */
+@Slf4j
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        log.info("origin:{}", request.getHeader("origin"));
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/github/shipengyan/framework/web/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 模块名
+ *
+ * @author spy
+ * @version 1.0 2018/11/1
+ * @since 1.0
+ */
+package com.github.shipengyan.framework.web;
